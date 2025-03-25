ca3344d9d2ad75f0c598f791ec130dedbc5be52b
Cameren Green <greencmaeren@gmail.com>
2025-03-25 13:09:46 -0500

    Update temperature.h

--- temperature.h
+++ temperature.h
@@ -1,7 +1,21 @@
-
 #ifndef TEMPERATURE_H
 #define TEMPERATURE_H
 
+// Conversion functions
 float celsius_to_fahrenheit(float celsius);
+float fahrenheit_to_celsius(float fahrenheit);
+float celsius_to_kelvin(float celsius);
+float kelvin_to_celsius(float kelvin);
+float fahrenheit_to_kelvin(float fahrenheit);
+float kelvin_to_fahrenheit(float kelvin);
+
+// Categorization function
+void categorize_temperature(float celsius);
+
+// Validation function
+bool is_valid_kelvin(float kelvin);
+
+// Main program function
+void runTemperatureConverter();
 
 #endif
