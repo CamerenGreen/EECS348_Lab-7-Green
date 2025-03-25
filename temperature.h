d97c3afbad4362974bc9c3b3602004797c23a1cd
Cameren Green <greencmaeren@gmail.com>
2025-03-25 12:54:28 -0500

    Update temperature.h

--- temperature.h
+++ temperature.h
@@ -1,38 +1,21 @@
-#ifndef __REPORT_H__
-#define __REPORT_H__
+#ifndef TEMPERATURE_H
+#define TEMPERATURE_H
 
-/* Add any function signatures you need here but do not modify the existing ones */
-
-/**
- * @brief Convert temperature in Celsius to Fahrenheit
- * 
- * @param celsius
- * @return float
- */
+// Conversion functions
 float celsius_to_fahrenheit(float celsius);
-
-/**
- * @brief Convert temperature in Fahrenheit to Celsius
- * 
- * @param fahrenheit
- * @return float
- */
 float fahrenheit_to_celsius(float fahrenheit);
-
-/**
- * @brief Convert temperature in Celsius to Kelvin
- * 
- * @param celsius
- * @return float
- */
 float celsius_to_kelvin(float celsius);
+float kelvin_to_celsius(float kelvin);
+float fahrenheit_to_kelvin(float fahrenheit);
+float kelvin_to_fahrenheit(float kelvin);
 
-/**
- * @brief Convert temperature in Kelvin to Celsius
- * 
- * @param celsius
- * @return float
- */
-float kelvin_to_celsius(float celsius);
+// Categorization function
+void categorize_temperature(float celsius);
 
-#endif /* __REPORT_H__ */
+// Validation function
+bool is_valid_kelvin(float kelvin);
+
+// Main program function
+void runTemperatureConverter();
+
+#endif
