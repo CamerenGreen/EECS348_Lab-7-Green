674f6a314e084f953e1f8a4ac859d9ea2347e3ba
Cameren Green <greencmaeren@gmail.com>
2025-03-25 13:38:18 -0500

    Update football.h

--- football.h
+++ football.h
@@ -4,7 +4,4 @@
 // Function to find and print all possible combinations of scoring plays
 void findCombinations(int score);
 
-// Main function to run the NFL score calculator program
-void runNFLScoreCalculator();
-
 #endif
