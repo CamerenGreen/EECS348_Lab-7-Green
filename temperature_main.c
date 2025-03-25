9b641291c9780412dbf4245d2ec79b78946885a0
Cameren Green <greencmaeren@gmail.com>
2025-03-25 13:31:36 -0500

    Update temperature_main.c

--- temperature_main.c
+++ temperature_main.c
@@ -1,6 +1,16 @@
 #include "temperature.h"
+#include <stdio.h>
 
-int main(){
-    runTemperatureConverter();
+// Declarations from temperature.c
+extern float celsius_to_fahrenheit(float c);
+extern void categorize_temperature(float c);
+
+int main() {
+    float temp;
+    printf("Enter temperature in C: ");
+    scanf("%f", &temp);
+    
+    printf("%.2f°C = %.2f°F\n", temp, celsius_to_fahrenheit(temp));
+    categorize_temperature(temp);
     return 0;
 }
